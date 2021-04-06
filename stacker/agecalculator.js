2f17ed1b7ee12bf7d02b0b1700f798c44c0c3393
Seble Aygoda <saygoda@mum.edu>
2021-04-06 14:58:51 -0500

    stacker exercise

---
+++ stacker/agecalculator.js
@@ -0,0 +1,4 @@
+let age=prompt('enter your age');
+let year=365;
+let days_lived=age*year;
+alert('you have lived'+days_lived)
---
+++ stacker/stacker.js
@@ -0,0 +1,5 @@
+
+let fname=prompt('first name');
+let lname=prompt('your last name');
+let age=prompt('your age');
+console.log(fname+''+lname+''+age)
